59404c3763867f72d4a587892d49f1fd8bb735f8
Alfredo Ramos <alfredo.ramos@yandex.com>
2017-12-12 21:34:00 -0600

    Fix tests

--- test/test_css.rb
+++ test/test_css.rb
@@ -9,15 +9,37 @@ class TestCss < Minitest::Test
     # Load tasks
     load('Rakefile', true)
 
+    # Build path
+    @path = 'build'
+
+    # Output filenames
+    @files = ['clean-reddit-theme.css', 'clean-reddit-theme.min.css']
+    @files.map! { |file| File.join(@path, file) }
+
+    # Minumum file size (13 KiB)
+    @size = (13 * 1024)
+  end
+
+  def teardown
     # Clean build
-    FileUtils.rm_r('build', force: true, secure: true) if Dir.exist?('build')
+    FileUtils.rm(@files, force: true)
   end
 
   def test_css_file
-    # Create CSS file
+    assert !File.exist?(@files.first)
+
+    Rake::Task['build:css'].invoke
+
+    assert File.exist?(@files.first)
+    assert_operator File.size(@files.first), :>=, @size
+  end
+
+  def test_minified_css_file
+    assert !File.exist?(@files.last)
+
     Rake::Task['build:minified'].invoke
 
-    # Check if file exists
-    assert File.exist?('build/clean-reddit-theme.min.css')
+    assert File.exist?(@files.last)
+    assert_operator File.size(@files.last), :>=, @size
   end
 end
